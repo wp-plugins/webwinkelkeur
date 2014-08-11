e01ec61ff7655558c326e085f2530d711e0f85ce
apeschar <apeschar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-11 15:37:08 +0000

    Update plugin
    git-svn-id: https://plugins.svn.wordpress.org/webwinkelkeur/trunk@963936 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- webwinkelkeur.php
+++ webwinkelkeur.php
@@ -3,7 +3,7 @@
 Plugin Name: WebwinkelKeur
 Plugin URI: https://www.webwinkelkeur.nl/webwinkel/mogelijkheden/wordpress-module/
 Description: De WordPress plugin zorgt voor een eenvoudige integratie van het WebwinkelKeur binnen jouw webwinkel. Hiermee is het heel eenvoudig om de innovatieve <a href="https://www.webwinkelkeur.nl/webwinkel/mogelijkheden/sidebar/">WebwinkelKeur Sidebar</a> in jouw WordPress website of WooCommerce webwinkel te integreren. Wanneer je WooCommerce gebruikt, kunnen er bovendien automatisch uitnodigingen naar je klanten worden gestuurd.
-Version: 1.1.0
+Version: 1.2.0
 Author: Albert Peschar
 Author URI: https://peschar.net/
 */
